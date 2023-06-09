0fa22a5d3754439d5309be12297e4cffa9e334c0
Ihsan Ullah Khan <ihsankhan@dgist.ac.kr>
2023-06-09 16:16:21 +0900

    Update Stream_CV2_Video.py

--- Stream_CV2_Video.py
+++ Stream_CV2_Video.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan  7 20:45:26 2021
-
-@author: malraharsh
-"""
 import cv2
 import streamlit as st
 
@@ -18,4 +12,3 @@ while run:
     FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
-    
